d17097b66c717fcb228676132730c7feab14c085
Harold Asai <koichiroasai@gmail.com>
2017-05-21 13:08:36 -0700

    bloccit-assignment20 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'questions/new'
+
+  get 'questions/edit'
+
   resources :posts
+
+  resources :questions
 
   get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,42 @@
 require 'random_data'
 
 # Create Posts
-50.times do
+#50.times do
 # #1
-  Post.create!(
+#  Post.create!(
 # #2
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-  )
-end
-posts = Post.all
+#    title:  RandomData.random_sentence,
+#    body:   RandomData.random_paragraph
+#  )
+#end
+#posts = Post.all
  
 # Create Comments
 # #3
-100.times do
-  Comment.create!(
+#100.times do
+#  Comment.create!(
 # #4
-    post: posts.sample,
-    body: RandomData.random_paragraph
+#    post: posts.sample,
+#    body: RandomData.random_paragraph
+#  )
+#end
+
+#puts "Seed finished"
+#puts "#{Post.count} posts created"
+#puts "#{Comment.count} comments created"
+
+
+# Create Posts
+10.times do
+# #1
+  Question.create!(
+# #2
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    resolved: false
   )
 end
+questions = Question.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Question.count} questions in total."
