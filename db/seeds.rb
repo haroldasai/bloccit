b4e98b337fbc689b384ea4e3bc2abdb02439c902
Harold Asai <koichiroasai@gmail.com>
2017-05-26 17:55:33 -0700

    bloccit-checkpoint27-posts_and_users completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
 require 'random_data'
+# Create Users
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -11,6 +22,7 @@ topics = Topic.all
 50.times do
 # #1
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -28,7 +40,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'koichiroasai@gmail.com', # replace this with your personal email
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
